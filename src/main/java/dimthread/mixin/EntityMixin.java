e88a7bbd86183b85599835b4af764b97559817a1
KaptainWutax <kaptainwutax@gmail.com>
2020-09-17 11:51:02 -0400

    added comments

--- src/main/java/dimthread/mixin/EntityMixin.java
+++ src/main/java/dimthread/mixin/EntityMixin.java
@@ -2,6 +2,7 @@ package dimthread.mixin;
 
 import dimthread.DimThread;
 import net.minecraft.entity.Entity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,6 +15,17 @@ public abstract class EntityMixin {
 
 	@Shadow public abstract Entity moveToWorld(ServerWorld destination);
 
+	/**
+	 * Moving entities between dimensions on the main server thread to not cause
+	 * chunk loading deadlocks or concurrent modification issues.
+	 *
+	 * This method is allowed to return null client-side. Modders who call this
+	 * function and use the return value should explicitly run a null check instead
+	 * of a {@code isClient} check.
+	 *
+	 * @see Entity#moveToWorld(ServerWorld)
+	 * @see ServerWorld#onPlayerChangeDimension(ServerPlayerEntity)
+	 * */
 	@Inject(method = "moveToWorld", at = @At("HEAD"), cancellable = true)
 	public void moveToWorld(ServerWorld destination, CallbackInfoReturnable<Entity> ci) {
 		if(DimThread.owns(Thread.currentThread())) {
--- src/main/java/dimthread/thread/ThreadPool.java
+++ src/main/java/dimthread/thread/ThreadPool.java
@@ -31,7 +31,7 @@ public class ThreadPool {
 
         this.executor.execute(() -> {
             action.run();
-            activeCount.decrement();
+            this.activeCount.decrement();
         });
     }
 
