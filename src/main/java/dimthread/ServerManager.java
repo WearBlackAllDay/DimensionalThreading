7146f27cc89ec593c5d6ac3cd3a9b7d26b60e1b1
Lara <69314462+WearBlackAllDay@users.noreply.github.com>
2020-10-13 15:03:24 +0200

    bump and namefix

--- src/main/java/dimthread/ServerManager.java
+++ src/main/java/dimthread/ServerManager.java
@@ -12,30 +12,29 @@ import java.util.WeakHashMap;
 
 public class ServerManager {
 
-	private final Map<MinecraftServer, Boolean> ACTIVES = Collections.synchronizedMap(new WeakHashMap<>());
-	private final Map<MinecraftServer, ThreadPool> THREAD_POOLS = Collections.synchronizedMap(new WeakHashMap<>());
+	private final Map<MinecraftServer, Boolean> actives = Collections.synchronizedMap(new WeakHashMap<>());
+	private final Map<MinecraftServer, ThreadPool> threadPools = Collections.synchronizedMap(new WeakHashMap<>());
 
 	public boolean isActive(MinecraftServer server) {
-		return this.ACTIVES.computeIfAbsent(server, s -> s.getGameRules().get(ModGameRules.ACTIVE.getKey()).get());
+		return this.actives.computeIfAbsent(server, s -> s.getGameRules().get(ModGameRules.ACTIVE.getKey()).get());
 	}
 
 	public void setActive(MinecraftServer server, GameRules.BooleanRule value) {
-		this.ACTIVES.put(server, value.get());
+		this.actives.put(server, value.get());
 	}
 
 	public ThreadPool getThreadPool(MinecraftServer server) {
-		return this.THREAD_POOLS.computeIfAbsent(server, s -> new ThreadPool(s.getGameRules().get(ModGameRules.THREAD_COUNT.getKey()).get()));
+		return this.threadPools.computeIfAbsent(server, s -> new ThreadPool(s.getGameRules().get(ModGameRules.THREAD_COUNT.getKey()).get()));
 	}
 
 	public void setThreadCount(MinecraftServer server, GameRules.IntRule value) {
-		ThreadPool current = this.THREAD_POOLS.get(server);
+		ThreadPool current = this.threadPools.get(server);
 
 		if(current.getActiveCount() != 0) {
 			throw new ConcurrentModificationException("Setting the thread count in wrong phase");
 		}
 
-		this.THREAD_POOLS.put(server, new ThreadPool(value.get()));
+		this.threadPools.put(server, new ThreadPool(value.get()));
 		current.shutdown();
 	}
-
 }
