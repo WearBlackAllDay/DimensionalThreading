7d5aca56a47ee79f5abd4f1176050567d4e13154
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2020-12-02 02:26:28 -0600

    Fixes #6

--- src/main/java/dimthread/mixin/ServerChunkManagerMixin.java
+++ src/main/java/dimthread/mixin/ServerChunkManagerMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ServerChunkManager.class)
+@Mixin(value = ServerChunkManager.class, priority = 1001)
 public abstract class ServerChunkManagerMixin extends ChunkManager implements IMutableMainThread {
 
 	@Shadow @Final @Mutable private Thread serverThread;
