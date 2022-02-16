e0560616197c75601a86fac09d68ff84a1ebc4fc
xiaoyu2006 <caoyi06@qq.com>
2022-02-17 00:38:16 +0800

    Attempt

--- src/main/java/wearblackallday/dimthread/mixin/EntityMixin.java
+++ src/main/java/wearblackallday/dimthread/mixin/EntityMixin.java
@@ -1,18 +1,26 @@
 package wearblackallday.dimthread.mixin;
 
-import wearblackallday.dimthread.DimThread;
-import net.minecraft.entity.Entity;
-import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.FallingBlockEntity;
+import net.minecraft.server.world.ServerWorld;
+import wearblackallday.dimthread.DimThread;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
 
 	@Shadow public abstract Entity moveToWorld(ServerWorld destination);
+    @Shadow @Final public abstract boolean isRemoved();
+    @Shadow private Entity.RemovalReason removalReason;
+
+    public Entity.RemovalReason formerRemovalReason = null;
 
 	/**
 	 * Schedules moving entities between dimensions to the server thread. Once all the world finish ticking,
@@ -31,4 +39,27 @@ public abstract class EntityMixin {
 		}
 	}
 
+	/**
+	 * If in the moveToWorld method removalReason is DISCARDED, then we assume its caused by a
+     * sand-duper and set the removalReason to null to trick the game it's not removed and hopefully
+     * we won't mess up with the newly-copied entity.
+	 */
+	@Redirect(method = "moveToWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isRemoved()Z"))
+	public final boolean forceNotRemoved(Entity entity) {
+		Entity.RemovalReason reason = this.removalReason;
+        if (((Object) this) instanceof FallingBlockEntity && reason == Entity.RemovalReason.DISCARDED) {
+			this.formerRemovalReason = reason;
+			reason = null;
+			return false;
+		}
+		return this.isRemoved();
+	}
+
+	@Inject(method = "moveToWorld", at = @At("TAIL"))
+	public void restoreRemovalReason(ServerWorld destination, CallbackInfoReturnable<Entity> ci) {
+		if (this.formerRemovalReason != null) {
+			this.removalReason = this.formerRemovalReason;
+			this.formerRemovalReason = null;
+		}
+	}
 }
