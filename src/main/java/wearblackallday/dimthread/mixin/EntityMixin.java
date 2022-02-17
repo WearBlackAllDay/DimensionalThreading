472ff453a2a7dcf20f2bf463315eb7b1961de006
xiaoyu2006 <caoyi06@qq.com>
2022-02-17 21:13:25 +0800

    Simpify

--- src/main/java/wearblackallday/dimthread/mixin/EntityMixin.java
+++ src/main/java/wearblackallday/dimthread/mixin/EntityMixin.java
@@ -17,49 +17,41 @@ import wearblackallday.dimthread.DimThread;
 public abstract class EntityMixin {
 
 	@Shadow public abstract Entity moveToWorld(ServerWorld destination);
-    @Shadow @Final public abstract boolean isRemoved();
-    @Shadow private Entity.RemovalReason removalReason;
 
-    public Entity.RemovalReason formerRemovalReason = null;
+	@Shadow @Final public abstract boolean isRemoved();
+
+	@Shadow private Entity.RemovalReason removalReason;
 
 	/**
-	 * Schedules moving entities between dimensions to the server thread. Once all the world finish ticking,
-	 * {@code moveToWorld()} is processed in a safe manner avoiding concurrent modification exceptions.
+	 * Schedules moving entities between dimensions to the server thread. Once all
+	 * the world finish ticking, {@code moveToWorld()} is processed in a safe manner
+	 * avoiding concurrent modification exceptions.
 	 *
-	 * For example, the entity list is not thread-safe and modifying it from multiple threads will cause
-	 * a crash. Additionally, loading chunks from another thread will cause a deadlock in the server chunk manager.
-	 * */
+	 * For example, the entity list is not thread-safe and modifying it from
+	 * multiple threads will cause a crash. Additionally, loading chunks from
+	 * another thread will cause a deadlock in the server chunk manager.
+	 */
 	@Inject(method = "moveToWorld", at = @At("HEAD"), cancellable = true)
 	public void moveToWorld(ServerWorld destination, CallbackInfoReturnable<Entity> ci) {
-		if(!DimThread.MANAGER.isActive(destination.getServer()))return;
+		if (!DimThread.MANAGER.isActive(destination.getServer()))
+			return;
 
-		if(DimThread.owns(Thread.currentThread())) {
+		if (DimThread.owns(Thread.currentThread())) {
 			destination.getServer().execute(() -> this.moveToWorld(destination));
 			ci.setReturnValue(null);
 		}
 	}
 
 	/**
-	 * If in the moveToWorld method removalReason is DISCARDED, then we assume its caused by a
-     * sand-duper and set the removalReason to null to trick the game it's not removed and hopefully
-     * we won't mess up with the newly-copied entity.
+	 * If in the moveToWorld method removalReason is DISCARDED, then we assume its
+	 * caused by a sand duplicator.
 	 */
 	@Redirect(method = "moveToWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isRemoved()Z"))
 	public final boolean forceNotRemoved(Entity entity) {
 		Entity.RemovalReason reason = this.removalReason;
-        if (((Object) this) instanceof FallingBlockEntity && reason == Entity.RemovalReason.DISCARDED) {
-			this.formerRemovalReason = reason;
-			reason = null;
+		if ((((Object) this) instanceof FallingBlockEntity) && (reason == Entity.RemovalReason.DISCARDED)) {
 			return false;
 		}
-		return this.isRemoved();
-	}
-
-	@Inject(method = "moveToWorld", at = @At("TAIL"))
-	public void restoreRemovalReason(ServerWorld destination, CallbackInfoReturnable<Entity> ci) {
-		if (this.formerRemovalReason != null) {
-			this.removalReason = this.formerRemovalReason;
-			this.formerRemovalReason = null;
-		}
+		return entity.isRemoved();
 	}
 }
