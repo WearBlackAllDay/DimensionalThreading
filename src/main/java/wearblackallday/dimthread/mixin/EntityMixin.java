8eb70c92b19f1a69942dfa11780a135049b83cdd
CaveNightingale <cavenightingale@foxmail.com>
2022-10-03 11:55:26 +0800

    Fix the bug that handed item disappeared when teleporting via nether portal

--- src/main/java/wearblackallday/dimthread/mixin/EntityMixin.java
+++ src/main/java/wearblackallday/dimthread/mixin/EntityMixin.java
@@ -1,29 +1,41 @@
 package wearblackallday.dimthread.mixin;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.FallingBlockEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import wearblackallday.dimthread.DimThread;
 
-import org.slf4j.LoggerFactory;
-
 @Mixin(Entity.class)
-public abstract class EntityMixin implements Cloneable {
-
-	public boolean isCloned = false;
-
-	@Shadow @Final
-	abstract void setRemoved(Entity.RemovalReason reason);
+public abstract class EntityMixin {
 
 	@Shadow
 	abstract void removeFromDimension();
+
+	private NbtCompound nbtCachedForMoveToWorld;
+
+	@Shadow public abstract World getWorld();
+
+	@Shadow public abstract NbtCompound writeNbt(NbtCompound nbt);
+
+	@Shadow public abstract @Nullable Entity moveToWorld(ServerWorld destination);
+
+	@Shadow private int netherPortalCooldown;
+
+	@Shadow protected BlockPos lastNetherPortalPosition;
+
+	@Shadow public abstract boolean isRemoved();
 
 	/**
 	 * Schedules moving entities between dimensions to the server thread. Once all
@@ -35,20 +47,14 @@ public abstract class EntityMixin implements Cloneable {
 	 * another thread will cause a deadlock in the server chunk manager.
 	 */
 	@Inject(method = "moveToWorld", at = @At("HEAD"), cancellable = true)
-	public void moveToWorld(ServerWorld destination, CallbackInfoReturnable<Entity> ci) {
+	public void onMoveToWorld(ServerWorld destination, CallbackInfoReturnable<Entity> ci) {
 		if (!DimThread.MANAGER.isActive(destination.getServer()))
 			return;
 
 		if (DimThread.owns(Thread.currentThread())) {
-			Entity snapshot = null;
-			try {
-				snapshot = (Entity) (this.clone());
-			} catch (CloneNotSupportedException e) {
-				throw new RuntimeException(e);
-			}
-			final Entity finalSnapshot = snapshot;
+			nbtCachedForMoveToWorld = writeNbt(new NbtCompound());
 			destination.getServer().execute(
-					() -> finalSnapshot.moveToWorld(destination)
+					() -> this.moveToWorld(destination)
 			);
 			this.removeFromDimension();
 			ci.setReturnValue(null);
@@ -56,19 +62,29 @@ public abstract class EntityMixin implements Cloneable {
 	}
 
 	/**
-	 * @author xiaoyu2006
-	 * @reason If this is a cloned entity, it should not execute removeFromDimension.
+	 * Perform deep copy instead of clone() to fix the bug that handed item disappear while teleporting
 	 */
+	@Redirect(method = "moveToWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;copyFrom(Lnet/minecraft/entity/Entity;)V"))
+	private void onMoveToWorldCopyFrom(Entity instance, Entity original) {
+		NbtCompound nbtCompound = nbtCachedForMoveToWorld;
+		nbtCompound.remove("Dimension");
+		instance.readNbt(nbtCompound);
+		((EntityMixin) (Object) instance).netherPortalCooldown = ((EntityMixin) (Object) original).netherPortalCooldown;
+		((EntityMixin) (Object) instance).lastNetherPortalPosition = ((EntityMixin) (Object) original).lastNetherPortalPosition;
+	}
+
 	@Inject(method = "removeFromDimension", at = @At("HEAD"), cancellable = true)
-	public void clonedDoNotRemove(CallbackInfo ci) {
-		if (this.isCloned) {
+	private void onRemoveFromDimension(CallbackInfo ci) {
+		if(isRemoved()) {
 			ci.cancel();
 		}
 	}
 
-	protected Object clone() throws CloneNotSupportedException {
-		EntityMixin cloned = (EntityMixin) super.clone();
-		cloned.isCloned = true;
-		return cloned;
+	/**
+	 * We check this because when we call this method in getServer().execute(), the entity has already been removed
+	 */
+	@Redirect(method = "moveToWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isRemoved()Z"))
+	private boolean onMoveToWorldIsRemoved(Entity instance) {
+		return instance.isRemoved() && ((EntityMixin) (Object) instance).nbtCachedForMoveToWorld == null;
 	}
 }
